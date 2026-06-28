976176ee3d2eae43c8adaaccc9d85fe0802849a8
Smriti Asthana <smrtsthn@gmail.com>
2026-06-29 01:05:48 +0530

    Use CPU-only PyTorch for deployment

--- app/services/embedding_service.py
+++ app/services/embedding_service.py
@@ -1,12 +1,23 @@
 import os 
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+model = None
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    return model
 
 def get_embedding(text):
-
-    embedding = model.encode(text)
+    embedding_model = get_model()
+    embedding = embedding_model.encode(
+        text
+    )
 
     return embedding.tolist()
+
